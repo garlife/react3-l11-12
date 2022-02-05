aa04a72652986a1fd38fdf85876d5d4a6cb00496
garlife <garlife@gmail.com>
2022-02-05 11:16:23 +0300

    не используйте useLayoutEffect (работает синхронно)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useEffect, useLayoutEffect } from 'react';
 import './style.css';
 
-const initialState = { age: 34 };
+const initialState = {
+  age: 34,
+};
 
 // action - спец объект вида { type: "increment", age: #TODO}
 const reducer = (state, action) => {
@@ -14,14 +16,35 @@ const reducer = (state, action) => {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [name, setName] = useState('Саша');
   // const [age, setAge] = useState(34);
 
+  useLayoutEffect(() => {
+    document.title = `Привет, ${name}`;
+    // for(let i=0, i < 200000000; i++) {};
+    console.log('App', 'useEffect');
+  }, [name]);
+
+  useEffect(() => {
+    //код инициилизации/подписки на ресурсы componentDidMount, хук будет вызван один раз
+    return () => {
+      // освобождение ресурсов
+      //это событие componentWillUnmunt
+    }
+  }, [])
+
+  const handlerChangeName = (event) => {
+    setName(event.target.value);
+  };
+
   return (
-    <div>
+    <>
+      <h1>Привет, {name}</h1>
+      <input type="text" value={name} onChange={(e) => handlerChangeName(e)} />
       <h3>Возраст: {state.age}</h3>
       <p>
-        <button onClick={() => dispatch({type: "increment"})}>+</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>+</button>
       </p>
-    </div>
+    </>
   );
 }
