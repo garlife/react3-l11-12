8a78dc8adb7e8e8b6223700bf1b2aeb1df705cd8
garlife <garlife@gmail.com>
2022-02-05 10:25:11 +0300

    setState in array funct ()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const [age, setAge] = React.useState(34);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h3>Возраст: {age}</h3>
+      <p>
+        <button onClick={() => setAge(age + 1)}>+</button>
+      </p>
     </div>
   );
 }
