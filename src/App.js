9f64751bd7f94c37f5d066d61a37eb1ee56ee30e
garlife <garlife@gmail.com>
2022-02-05 10:43:31 +0300

    reducer v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './style.css';
 
+const initialState = { age: 34 };
+
+// action - спец объект вида { type: "increment", age: #TODO}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { age: state.age + 1 };
+  }
+  return initialState;
+};
+
 export default function App() {
-  const [age, setAge] = React.useState(34);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [age, setAge] = useState(34);
 
   return (
     <div>
-      <h3>Возраст: {age}</h3>
+      <h3>Возраст: {state.age}</h3>
       <p>
-        <button onClick={() => setAge((a) => a + 1)}>+</button>
+        <button onClick={() => dispatch({type: "increment"})}>+</button>
       </p>
     </div>
   );
