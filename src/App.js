db0b7b40e5b8cbef3d113963fdfd9ba9404cdad3
garlife <garlife@gmail.com>
2022-02-05 12:15:58 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import React, { useState, useMemo, useCallBack } from 'react';
+import React, { useState, useCallback, useEffect, useDebugValue } from 'react';
 import './style.css';
 
-function createDisplayName(_name, _surName){
-  const user = { name: _name, surName: _surName };
-  console.log(user);
-  return user;
+function useAddConsoleLog(value){
+  const [msg, setMsg] = useState(value);
+  // для dev tools debugging
+  const addLog = useCallback( (_msg) => (console.log(_msg)))
+  useEffect(() =>{addLog(msg)}, [msg, addLog] );
+  useDebugValue("Это моя функция, возвращает имя");
+  return;
   
 }
 
 export default function App() {
-  const [name, setName] = useState('Саша');
-  const [surName, setSurName] = useState('Романов');
-  const [age, setAge] = useState('34');
-  const displayName = useMemo(
-    () => createDisplayName(name, surName), [name, surName]
-    );
- 
+  const [age, setAge] = useState(34);
+  useAddConsoleLog("Hi!");
 
-  //memoization - возвращает те же самые значения функции
-  //ниже поломанное решение, требует фикса
+  
+ 
   return (
     <>
-      <h1>
-        Привет, {name} {surName}
-      </h1>
-      <input type="text" value={name} onChange={
-        // setName(() => {event.target.value})
-        } />
-
       <h3>Возраст: {age}</h3>
       <p>
         <button onClick={() => setAge((a) => a + 1)}>+</button>
       </p>
-      {JSON.stringify(displayName, null, 2)}
     </>
   );
 }
---
+++ src/useMemo.js
@@ -0,0 +1,38 @@
+import React, { useState, useMemo, useCallBack } from 'react';
+import './style.css';
+
+function createDisplayName(_name, _surName){
+  const user = { name: _name, surName: _surName };
+  console.log(user);
+  return user;
+  
+}
+
+export default function App() {
+  const [name, setName] = useState('Саша');
+  const [surName, setSurName] = useState('Романов');
+  const [age, setAge] = useState(34);
+  const displayName = useMemo(
+    () => createDisplayName(name, surName), [name, surName]
+    );
+ 
+
+  //memoization - возвращает те же самые значения функции
+  //ниже поломанное решение, требует фикса
+  return (
+    <>
+      <h1>
+        Привет, {name} {surName}
+      </h1>
+      <input type="text" value={name} onChange={
+        // setName(() => {event.target.value})
+        } />
+
+      <h3>Возраст: {age}</h3>
+      <p>
+        <button onClick={() => setAge((a) => a + 1)}>+</button>
+      </p>
+      {JSON.stringify(displayName, null, 2)}
+    </>
+  );
+}
